ebf386b2531a4ed7c0db08cd63005fe4b026787e
skill-informatic <mariojhaziel04@gmail.com>
2026-05-14 17:03:08 -0600

    actualizacion de cv

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const openModal = document.querySelector('#cv');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
+const openModal = document.querySelector("#cv");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal__close");
 
-
-openModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.add('modal--show')
+openModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("click modal");
+  modal.classList.add("modal--show");
 });
 
-closeModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal--show');
-})
+closeModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.remove("modal--show");
+});
